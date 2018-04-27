61a49da0d2ef6e6dd0dd1d7a8b890ade0fbc9f4d
andrey-nosan <andrey.nosan@gmail.com>
2018-04-28 00:27:15 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore.loc/admin/');
+define('HTTP_CATALOG', 'http://ocstore.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore.loc/admin/');
+define('HTTPS_CATALOG', 'http://ocstore.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/ocstore.loc/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/ocstore.loc/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/ocstore.loc/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/ocstore.loc/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/ocstore.loc/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/ocstore.loc/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/ocstore.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/ocstore.loc/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/ocstore.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/ocstore.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/ocstore.loc/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/ocstore.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ocstore');
+define('DB_PASSWORD', 'temp123');
+define('DB_DATABASE', 'ocstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
